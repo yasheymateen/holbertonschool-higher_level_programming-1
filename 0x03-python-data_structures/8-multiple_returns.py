68620c9d24b31d98fada103f0518c75255f2f27d
Jinji Zhang <362@holbertonschool.com>
2018-09-18 21:37:34 +0000

    Fix: Return None if empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     if sentence is None:
         return (0, None)
-    elif len(sentence) == 0:
-        return (0, "")
+    if len(sentence) == 0:
+        return (0, None)
     return (len(sentence), sentence[0])
