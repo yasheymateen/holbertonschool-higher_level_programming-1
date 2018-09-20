14b0968be819990faf781b98b6d28a90f631f015
Jinji Zhang <362@holbertonschool.com>
2018-09-20 21:39:07 +0000

    Add handling if argument is not a string or is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not string:
+        return 0
     def get_value(c):
         if c == 'I':
             return 1
