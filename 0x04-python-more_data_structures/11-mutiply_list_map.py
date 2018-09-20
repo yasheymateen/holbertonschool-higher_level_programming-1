5d132531d4840e11547ce4c0e8cebae9c81058c2
Jinji Zhang <362@holbertonschool.com>
2018-09-20 20:26:01 +0000

    Refactor: use lambda with map instead of passing a callback function

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    def multiply_by(x):
-        return x * number
-    new_list = list(map(multiply_by, my_list))
+    new_list = list(map(lambda x: x * number, my_list))
     return new_list
