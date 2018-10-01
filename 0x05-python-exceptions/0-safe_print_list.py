310702cd05aceb0c25c95c2c63c531405effd496
Jinji Zhang <362@holbertonschool.com>
2018-10-01 19:34:20 +0000

    Add handling to print all types of elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,11 +9,11 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         i = 0
         while (i < x):
-            print("{:d}".format(my_list[i]), end='')
+            print("{}".format(my_list[i]), end='')
             count += 1
             i += 1
         print()
         return count
-    except ValueError:
+    except:
         print()
         return count
