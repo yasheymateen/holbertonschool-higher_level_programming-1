18d8f1c31fd87620a26b75bccfb4ad4b7b95124f
Jinji Zhang <362@holbertonschool.com>
2018-10-02 23:04:30 +0000

    PEP8 style conform

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Empty class Square"""
+
+
 class Square:
     """Represents a class Square"""
     pass
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """class Square"""
+
+
 class Square:
     """Represents a Square"""
 
