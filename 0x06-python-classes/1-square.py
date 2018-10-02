6aa1926ac0ba51608b3b6919c8af1bb8e63837c1
Jinji Zhang <362@holbertonschool.com>
2018-10-02 20:38:30 +0000

    Square class with size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Square"""
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
