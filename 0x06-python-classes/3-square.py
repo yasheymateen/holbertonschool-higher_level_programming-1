d18e1e82c12de4c657790d85f21d7003b5cd4b49
Jinji Zhang <362@holbertonschool.com>
2018-10-02 21:17:24 +0000

    Add: public instance method area()

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Square"""
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size=0):
+        """Initializes the data with error handling."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
