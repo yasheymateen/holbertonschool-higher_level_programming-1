c37ac71fcafa4d577dc918a9e6aeb5e279d76824
Jinji Zhang <362@holbertonschool.com>
2018-10-03 02:22:59 +0000

    Removed excess code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,10 +12,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if not isinstance(position[0], int) or position[0] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(position[1], int) or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     def area(self):
