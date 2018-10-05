15f6df1355e4fd7f04e6e4b9b9a1797ef1d529ce
Jinji Zhang <362@holbertonschool.com>
2018-10-05 17:30:52 +0000

    Code refactor

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,11 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the data with error handling."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     def area(self):
         """Returns the current square area."""
