7d55a4d7a7cde6b73d840439c8f797281285b109
Jinji Zhang <362@holbertonschool.com>
2018-10-06 19:07:27 +0000

    add test for list of one element

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,6 +13,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 4.5, 3, 2.3]), 4.5)
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([-4, 3, -2, 1]), 3)
+        self.assertEqual(max_integer([1]), 1)
 
         with self.assertRaises(TypeError):
             max_integer([3, '2', 1])
