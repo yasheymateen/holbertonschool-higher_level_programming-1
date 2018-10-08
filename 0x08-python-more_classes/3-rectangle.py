ea3c4d3fb8dbb7a2b49e68990be2db984cdf0677
Jinji Zhang <362@holbertonschool.com>
2018-10-08 18:55:03 +0000

    Add __str__ method to print representation of rectangle with char '#'

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Rectangle module
+"""
+
+
+class Rectangle:
+    """class Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """Initializes instance of Rectangle. If either width or height
+        are ommitted, they are set to 0 by default
+        """
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """Print the rectangle with the character #
+        """
+        height = self.height
+        width = self.width
+        if height != 0 and width != 0:
+            for i in range(height):
+                for j in range(width):
+                    print('#', end='')
+                if i != height - 1:
+                    print()
+        return ""
+
+    @property
+    def width(self):
+        """Returns the instance attribute width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Returns the instance attribute height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Sets the width to positive integer value
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the height to positive integer value
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns perimeter of the rectangle
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
