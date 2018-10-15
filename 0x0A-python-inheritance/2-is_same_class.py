58658a4e1b751fcef9b431f63f5ff09b8d1d91ed
Jinji Zhang <362@holbertonschool.com>
2018-10-15 19:34:15 +0000

    Add: function that returns Boolean if obj is exactly an instance of specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""is_same_class function
+"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance of the specified class
+    otherwise False
+    """
+    return type(obj) is a_class
