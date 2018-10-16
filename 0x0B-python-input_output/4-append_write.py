c71293a06c3e62299a803b4c0b1e67f5ae5daf2a
Jinji Zhang <362@holbertonschool.com>
2018-10-16 19:23:54 +0000

    1st attempt at function that appends txt to a file

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""append_write module"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text file.
+    Returns the number of characters added
+    """
+    with open(filename, "a") as f:
+        chars = f.write(text)
+    return chars
