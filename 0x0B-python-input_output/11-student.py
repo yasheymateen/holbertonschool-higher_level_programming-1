d06855a9051e859ee1504534b4350318eb167327
Jinji Zhang <362@holbertonschool.com>
2018-10-16 21:57:20 +0000

    1st attempt at Creating student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student instance with first_name, last_name, and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance
+        """
+        return self.__dict__
