0e8fc4fe1ee42bd4b3016ac2050da64877d7af2a
Jinji Zhang <362@holbertonschool.com>
2018-10-17 16:37:10 +0000

    Shortened way extra code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,10 +9,4 @@ class MyList(list):
     def print_sorted(self):
         """prints the list in sorted ascending order
         """
-        print('[', end='')
-        for i in sorted(self):
-            if i != sorted(self)[len(self) - 1]:
-                print(i, end=', ')
-            else:
-                print(i, end='')
-        print(']')
+        print(sorted(self))
