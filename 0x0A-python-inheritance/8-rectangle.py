e146e9b9b25d3c8750df7371a455cec15e92e3f9
Jinji Zhang <362@holbertonschool.com>
2018-10-17 17:34:09 +0000

    Use import to import BaseGeometry parent class instead of keeping hard code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,22 +3,7 @@
 """
 
 
-class BaseGeometry:
-    """class BaseGeometry
-    """
-    def area(self):
-        """Public instance method that raises an Exception
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that value is an integer
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """class Rectangle, inherits from class BaseGeometry
