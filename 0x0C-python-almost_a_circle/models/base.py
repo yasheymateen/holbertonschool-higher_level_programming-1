bc0167869ca917e00971be8581cad9fe52646a2f
Jinji Zhang <362@holbertonschool.com>
2018-10-19 21:18:18 +0000

    Add class method save_to_file which writes JSON string representation of a list of objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Base module"""
 import json
 
+
 class Base:
     """class Base
     """
@@ -32,3 +33,22 @@ class Base:
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method that writes the JSON string representation of
+        list_objs to a file.
+
+        Parameters:
+        list_objs: list of instances who inherits from Base
+        """
+        filename = cls.__name__ + '.json'
+        if list_objs is None:
+            new_list = []
+        else:
+            new_list = []
+            for obj in list_objs:
+                new_list.append(obj.to_dictionary())
+        with open(filename, "w", encoding='utf8') as f:
+            json_string = Base.to_json_string(new_list)
+            f.write(json_string)
