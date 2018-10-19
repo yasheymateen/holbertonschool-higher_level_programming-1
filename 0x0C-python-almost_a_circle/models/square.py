b5a4fa47273d06cbb7dfd03938c256c649c5e27d
Jinji Zhang <362@holbertonschool.com>
2018-10-19 17:58:26 +0000

    Add update method for Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,39 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Public method that updates the class Square with *args or **kwargs
+
+        For *args:
+        1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+
+        IFF *args does not exist or is empty, then kwargs (a double
+        pointer to a dictionary of key-value pairs) is used to update
+        the Square instance
+        """
+        i = 0
+        if args and len(args) > 0:
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+                i += 1
+        else:
+            for key in kwargs:
+                if key == 'id':
+                    self.id = kwargs[key]
+                elif key == 'size':
+                    self.size = kwargs[key]
+                elif key == 'x':
+                    self.x = kwargs[key]
+                elif key == 'y':
+                    self.y = kwargs[key]
