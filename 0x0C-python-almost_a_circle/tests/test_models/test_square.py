82b0b52c721148c204a9c8a12ae2ae59cfef0869
Jinji Zhang <362@holbertonschool.com>
2018-10-22 23:50:45 +0000

    Add __str__ and update() unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -67,6 +67,9 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(self.c.x, 0)
         self.c.x = 98
         self.assertEqual(self.c.x, 98)
+        self.assertRaises(TypeError, self.c.x, "hi")
+        with self.assertRaises(ValueError):
+            self.c.x = -1
 
     def test_y(self):
         """test for y"""
@@ -75,3 +78,80 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(self.c.y, 0)
         self.c.y = 96
         self.assertEqual(self.c.y, 96)
+        self.assertRaises(TypeError, self.c.y, "hi")
+        with self.assertRaises(ValueError):
+            self.c.y = -1
+
+    def test_str_method(self):
+        """test for str method"""
+        expected = "[Square] (1) 0/0 - 2\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        expected = "[Square] (100) 1/2 - 3\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.b)
+        self.assertEqual(expected, output.getvalue())
+
+        expected = "[Square] (2) 0/0 - 5\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.c)
+        self.assertEqual(expected, output.getvalue())
+
+    def test_square_update(self):
+        """test for square method update"""
+        self.a.update(10)
+        expected = "[Square] (10) 0/0 - 2\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        self.a.update(1, 3)
+        expected = "[Square] (1) 0/0 - 3\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        self.a.update(1, 2, 3)
+        expected = "[Square] (1) 3/0 - 2\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        self.a.update(1, 2, 3, 4)
+        expected = "[Square] (1) 3/4 - 2\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        self.a.update(x=12)
+        expected = "[Square] (1) 12/4 - 2\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        self.a.update(size=7, id=89, y=1)
+        expected = "[Square] (89) 12/1 - 7\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        self.a.update(999, x=200, size=2)
+        expected = "[Square] (999) 12/1 - 7\n"
+        output = io.StringIO()
+        sys.stdout = output
+        print(self.a)
+        self.assertEqual(expected, output.getvalue())
+
+        with self.assertRaises(ValueError):
+            self.a.update(1, -1)
