61b3a5e0f8a1a163d06344ecdc274e8873645326
Jinji Zhang <362@holbertonschool.com>
2018-12-05 17:34:39 +0000

    Remove extra faulty line

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,3 @@
 -- (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table MODIFY name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
