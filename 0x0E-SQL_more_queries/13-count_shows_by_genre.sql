c275818cc62d694fb78482db11af776a44eecda3
Jinji Zhang <362@holbertonschool.com>
2018-12-05 00:37:29 +0000

    Change join type

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- and displays the number of shows linked to each.
 SELECT tv_genres.name 'genre', COUNT(tv_genres.name) 'number_of_shows'
 FROM tv_genres
-LEFT JOIN tv_show_genres
+JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
