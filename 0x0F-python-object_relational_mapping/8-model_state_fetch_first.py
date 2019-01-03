912a01e377bac7526726e603c5a18bf6b255faba
Jinji Zhang <362@holbertonschool.com>
2019-01-03 21:15:25 +0000

    Fix: pep8, prints nothing if states table is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.id==1):
+    for instance in session.query(State).filter(State.id == 1):
         print("{}: {}".format(instance.id, instance.name))
+    else:
+        print("Nothing")
