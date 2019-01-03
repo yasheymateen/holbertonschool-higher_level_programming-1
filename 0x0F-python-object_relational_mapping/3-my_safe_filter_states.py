d2ceb7c7b31be354cd81046e23b4d9192691cea1
Jinji Zhang <362@holbertonschool.com>
2019-01-03 00:11:06 +0000

    1st attempt at SQL query safe from SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Sript that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+SYNTAX: ./3-my_safe_filter_states.py mysql-username mysql-password
+database-name name-argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username, password, db_name, state_name_search = sys.argv[1:]
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+    WHERE name LIKE %s ORDER BY states.id""", (state_name_search,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
