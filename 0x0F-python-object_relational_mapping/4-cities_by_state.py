7a4d8819848c0fd1e8812476594cc65763cfec2c
Jinji Zhang <362@holbertonschool.com>
2019-01-03 18:52:34 +0000

    Add: case-sensitive comparison, close db and cursor

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Script that lists all states with name starting with the uppercase letter N
-Syntax: ./1-filter-states.py mysql-username mysql-password database-name
-"""
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
 
     username, password, db_name = sys.argv[1:]
+    cmd = """SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states ON state_id = states.id ORDER BY cities.id"""
     db = MySQLdb.connect(host="localhost",
                          user=username,
                          passwd=password,
                          db=db_name)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states"
-                "WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    cur.execute(cmd)
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
