d4f9d8a14b10e93e061bab1e99b08d180a83793f
Jinji Zhang <362@holbertonschool.com>
2019-01-21 21:11:29 +0000

    Fix: var name typo

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         retval = response.read()
-        print(retvl.decode())
+        print(retval.decode())
