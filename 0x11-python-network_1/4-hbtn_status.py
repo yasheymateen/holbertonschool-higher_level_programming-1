d07469472eedf94a8a6ad0711097f956176faed0
Jinji Zhang <362@holbertonschool.com>
2019-01-21 22:20:00 +0000

    Fix: code should not be executed when imported

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,8 @@
 """Fetch https://intranet.hbtn.io/status using the package requests"""
 import requests
 
-r = requests.get('https://intranet.hbtn.io/status')
-print('Body response:')
-print('\t- type:', type(r.text))
-print('\t- content:', r.text)
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,7 +4,7 @@ value of the variable X-Request-Id in the response header.
 """
 import requests
 import sys
-
-url = sys.argv[1]
-r = requests.get(url)
-print(r.headers['X-Request-Id'])
+if __name__ == '__main__':
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
