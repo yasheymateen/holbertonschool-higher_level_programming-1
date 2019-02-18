c1a28a902be220285be1700f9c7501a5ec4ef9da
Jinji Zhang <362@holbertonschool.com>
2019-02-18 23:14:46 +0000

    1st attempt

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
