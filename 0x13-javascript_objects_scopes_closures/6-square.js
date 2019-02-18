288868ba03434a4dd5d0a5d848fa8828f3a84ec2
Jinji Zhang <362@holbertonschool.com>
2019-02-18 23:04:27 +0000

    Fix: Import and inherit from previous Square class declaration

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,31 +1,8 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w && w > 0 && h && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
