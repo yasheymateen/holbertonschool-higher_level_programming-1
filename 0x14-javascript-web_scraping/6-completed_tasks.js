74bee0ea7172dba950e6bf0f7576e3f082c9ebd7
Jinji Zhang <362@holbertonschool.com>
2019-02-19 21:11:28 +0000

    Variable name change

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,12 +4,12 @@ const request = require('request');
 const options = {
   url: process.argv[2]
 };
-request.get(options, function (err, response, data) {
+request.get(options, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
     let completedTasks = {};
-    let json = JSON.parse(data);
+    let json = JSON.parse(body);
     for (let i = 0; i < json.length; i++) {
       if (!(json[i].userId in completedTasks)) {
         completedTasks[json[i].userId] = 0;
